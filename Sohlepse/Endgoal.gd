3d44c71d3a799bd23f9522761a64a9bb57e373ee
Pedro9Paulo <ppqbambace@yahoo.com.br>
2018-08-27 10:23:44 -0300

    fixed #12

--- Sohlepse/Endgoal.gd
+++ Sohlepse/Endgoal.gd
@@ -3,9 +3,11 @@ extends Area2D
 var inbody = null
 
 func _process(delta):
+	print(get_parent().get_node("EndgoalMirror").inEndGoal)
 	if inbody != null and inbody.get_name().begins_with("player") and Input.is_action_just_pressed("interact"):
-		global.current_stage += 1
-		get_tree().reload_current_scene()
+		if get_parent().get_node("EndgoalMirror") == null or get_parent().get_node("EndgoalMirror").inEndGoal:
+			global.current_stage += 1
+			get_tree().reload_current_scene()
 		
 func _on_Endgoal_body_entered(body):
 	inbody = body
