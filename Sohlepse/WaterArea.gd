1797b9895120a5eddb55be18d7bb50e91ecd3ed0
Larissa <larissa_sala@usp.br>
2018-08-29 15:54:18 -0300

    Pequenos ajustes

--- Sohlepse/WaterArea.gd
+++ Sohlepse/WaterArea.gd
@@ -4,7 +4,6 @@ func _on_Area_body_entered(body):
 	if (body.get_name().begins_with("player")):
 		body.terrain = 0.2
 		body.in_terrain = body.in_terrain + 1
-		print ("inside: " + str(body.in_terrain))
 
 
 func _on_Area_body_exited(body):
@@ -12,4 +11,3 @@ func _on_Area_body_exited(body):
 		body.in_terrain = body.in_terrain - 1
 		if (body.in_terrain == 0):
 			body.terrain = 1.0
-		print("out: " + str(body.in_terrain))
