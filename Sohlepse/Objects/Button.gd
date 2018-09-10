1a885d419976b3e73bbf9832c299113e073469e4
Larissa <larissa_sala@usp.br>
2018-09-10 12:27:16 -0300

    Paredes com estados e começando a fazer botões para dois objetos simultâneos

--- Sohlepse/Objects/Button.gd
+++ Sohlepse/Objects/Button.gd
@@ -1,14 +1,24 @@
 extends Area2D
 
-export var object = ""
+export var object = "" # Mandatory. Only needs the type of the object ('Wall'/'Thorns'), connects with the correct number right away
+export var object2 = "" # Is optional. Must be the exact name of the node you want to link
 
 #button1
 onready var bid = get_name().substr(6,2)
 onready var obj = get_parent().get_node(object+bid)
+var obj2
+
 signal hit
 	
 func _ready():
 	obj.connect("triggered", obj, "_on_" + object + "_triggered")
+	if object2 != "":
+		obj2 = get_parent().get_node(object2)
+		if object2.begins_with("Wall"):
+			print("denovo")
+			obj2.connect("triggered2", obj2, "_on_Wall_triggered2")
+		elif object2.begins_with("Thorns"):
+			obj2.connect("triggered2", obj2, "_on_Thorns_triggered2")
 	
 func _on_Button_body_entered(body):
 	$CollisionShape2D.disabled = true
@@ -17,3 +27,5 @@ func _on_Button_body_entered(body):
 
 func _on_Button_hit():
 	obj.emit_signal("triggered")
+	if object2 != "":
+		obj2.emit_signal("triggered2")
--- Sohlepse/Objects/Wall.gd
+++ Sohlepse/Objects/Wall.gd
@@ -1,12 +1,30 @@
 extends StaticBody2D
 
+export var begin = 0 # 0 = default is enabled
 signal triggered
+signal triggered2
 signal default
 
+func _ready():
+	if begin != 0:
+		self.hide()
+		$CollisionShape2D.disabled = true
+
 func _on_Wall_triggered():
-	self.hide()
-	$CollisionShape2D.disabled = true
-	
+	if begin == 0:
+		self.hide()
+		$CollisionShape2D.disabled = true
+	else:
+		self.show()
+		$CollisionShape2D.disabled = false
+		
+func _on_Wall_triggered2():
+	_on_Wall_triggered()
+
 func _on_Wall_default():
-	self.show()
-	$CollisionShape2D.disabled = false
+	if begin == 0:
+		self.show()
+		$CollisionShape2D.disabled = false
+	else:
+		self.hide()
+		$CollisionShape2D.disabled = true
