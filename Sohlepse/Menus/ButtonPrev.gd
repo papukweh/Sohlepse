7d42a654590d75066b9685339744b0d8702a2321
Pedro9Paulo <ppqbambace@yahoo.com.br>
2018-09-24 10:56:17 -0300

    correção do commit mais recente

--- Sohlepse/Menus/ButtonPrev.gd
+++ Sohlepse/Menus/ButtonPrev.gd
@@ -1,8 +1,10 @@
 extends Button
 
-var batch = get_parent().get_parent().batch
+onready var batch = get_parent().get_parent().batch
 
 func _on_ButtonPrev_button_down():
 	get_parent().hide()
 	get_parent().get_parent().get_node("Batch" + str(batch-1)).show()
 	get_parent().get_parent()._atualiza((batch-1)*6 )
+
+
