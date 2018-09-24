5da196515c7c0f0b32ff0a4989bf8f40b3011b61
Pedro9Paulo <ppqbambace@yahoo.com.br>
2018-09-24 10:26:09 -0300

    melhora no menu de fases

---
+++ Sohlepse/ButtonNext.gd
@@ -0,0 +1,13 @@
+extends Button
+
+var batch = get_parent().get_parent().batch
+func _ready():
+	if(batch*6 < global.unlocked_stage):
+		self.disabled = true
+
+func _on_ButtonNext_button_down():
+	
+	print(batch)
+	get_parent().hide()
+	get_parent().get_parent().get_node("Batch" + str(batch+1)).show()
+	get_parent().get_parent()._atualiza(batch*6 + 1)
---
+++ Sohlepse/Menus/ButtonPrev.gd
@@ -0,0 +1,8 @@
+extends Button
+
+var batch = get_parent().get_parent().batch
+
+func _on_ButtonPrev_button_down():
+	get_parent().hide()
+	get_parent().get_parent().get_node("Batch" + str(batch-1)).show()
+	get_parent().get_parent()._atualiza((batch-1)*6 )
