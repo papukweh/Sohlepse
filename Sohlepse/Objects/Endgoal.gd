b4347bfcc9b2661cd63c542bd60a6cdd9ba5c615
papukweh <nathalia.borin@usp.br>
2018-10-09 15:57:19 -0300

    Nova fase e endgoals caindo

--- Sohlepse/Objects/Endgoal.gd
+++ Sohlepse/Objects/Endgoal.gd
@@ -1,6 +1,9 @@
-extends Area2D
+extends KinematicBody2D
 
+export var invert_vertical = 1
 var inbody = null
+var GRAVITY = 700.0 # pixels/second/second
+var velocity = Vector2()
 
 func _process(delta):
 	if inbody != null and inbody.get_name().begins_with("Player") and Input.is_action_just_pressed("interact"):
@@ -9,9 +12,16 @@ func _process(delta):
 			global.save()
 			get_tree().reload_current_scene()
 
-		
+func _physics_process(delta):
+	var force = Vector2(0, invert_vertical * GRAVITY)
+	
+	velocity += force * delta	
+	velocity = move_and_slide(velocity, Vector2(0, -1))
+
 func _on_Endgoal_body_entered(body):
+	print("etnei")
 	inbody = body
 
 func _on_Endgoal_body_exited(body):
+	print("sai")
 	inbody = null
--- Sohlepse/Objects/EndgoalMirror.gd
+++ Sohlepse/Objects/EndgoalMirror.gd
@@ -1,6 +1,10 @@
-extends Area2D
+extends KinematicBody2D
 var inbodyM = null
 var inEndGoal = false
+
+export var invert_vertical = 1
+var GRAVITY = 700
+var velocity = Vector2()
 
 func _process(delta):
 	if inbodyM != null and inbodyM.get_name().begins_with("Player"):
@@ -8,7 +12,14 @@ func _process(delta):
 	else:
 		inEndGoal = false
 	pass
+
+func _physics_process(delta):
+	var force = Vector2(0, invert_vertical * GRAVITY)
 	
+	velocity += force * delta	
+	velocity = move_and_slide(velocity, Vector2(0, -1))
+
+
 func _on_EndgoalMirror_body_entered(body):
 	inbodyM = body
 	pass
