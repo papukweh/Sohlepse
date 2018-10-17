4222348c6054b23565554120bf55233282220cd4
Pedro9Paulo <ppqbambace@yahoo.com.br>
2018-10-17 13:30:07 -0300

    camera limits concertado

--- Sohlepse/Manager/Dual_screen.gd
+++ Sohlepse/Manager/Dual_screen.gd
@@ -16,29 +16,33 @@ func can_load(act, mode, invert):
 			Viewport2.render_target_v_flip = true
 		camera1.target = world.get_node("Players/Player1")
 		camera2.target = world.get_node("Players/Player2")
-		set_camera_limits(camera1, camera2)
+		set_camera1_limits(camera1)
+		set_camera2_limits(camera2, 0)
 	elif act == 3:
 		camera1.target = world.get_node("Players/Player1")
 		$Viewports/C2.queue_free()
 		$Viewports/Filter.queue_free()
 		$Viewports/Division.queue_free()
+		set_camera1_limits(camera1)
 	else:
 		Viewport2.world_2d = Viewport1.world_2d
 		camera1.target = world.get_node("Players/Player1")
 		camera2.target = world.get_node("Players/Player1")
-		Viewport2.render_target_v_flip = true
+		set_camera1_limits(camera1)
 		if mode == 1:
-			camera2.rotating = true
-		else: set_camera_limits(camera1, camera2)
-func set_camera_limits(camera1, camera2):
-	var map_limits = [world.get_node("Real").get_global_rect(), world.get_node("Mirrored").get_global_rect()] 
-	var cam = [camera1, camera2]
-	for i in [0,1]:
-		print(map_limits[i].position.x)
-		print(map_limits[i].end.x)
-		print(map_limits[i].position.y)
-		print(map_limits[i].end.y)
-		cam[i].limit_left = map_limits[i].position.x
-		cam[i].limit_right = map_limits[i].end.x
-		cam[i].limit_top = map_limits[i].position.y
-		cam[i].limit_bottom = map_limits[i].end.y
+			set_camera2_limits(camera2, 675)
+		else:
+			set_camera2_limits(camera2, 0)
+			Viewport2.render_target_v_flip = true 
+func set_camera1_limits(cam1):
+	var map_limits = world.get_node("Real").get_global_rect()
+	cam1.limit_left = map_limits.position.x
+	cam1.limit_right = map_limits.end.x
+	cam1.limit_top = map_limits.position.y
+	cam1.limit_bottom = map_limits.end.y
+func set_camera2_limits(cam2, desloc):
+	var map_limits = world.get_node("Mirrored").get_global_rect()
+	cam2.limit_left = map_limits.position.x - desloc
+	cam2.limit_right = map_limits.end.x + desloc
+	cam2.limit_top = map_limits.position.y
+	cam2.limit_bottom = map_limits.end.y
