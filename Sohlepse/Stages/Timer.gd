b071211ade39f3a4a658d7692d6a72176766343a
PauloPaulo <ppqbambace@yahoo.com.br>
2018-11-26 20:54:39 -0200

    animação de explosão

--- Sohlepse/Stages/Timer.gd
+++ Sohlepse/Stages/Timer.gd
@@ -1,6 +1,7 @@
 extends Timer
 var pan = null
 var ptimer = null
+var played = false
 
 func _ready():
 	pan = get_tree().get_root().get_child(1).get_node("Tutoriais")
@@ -8,12 +9,18 @@ func _ready():
 	pass
 
 func _process(delta):
+	if time_left <= 2 and !played:
+		print("ue")
+		pan.get_node("Bomb").play("Booom")
+		played = true
 	ptimer.label = time_the_timer()
 	ptimer.ready()
 	
 func time_the_timer():
-	if(time_left < 10):
+	if time_left < 10:
 		return "0:0" + str(int(time_left)%60)
+	elif time_left >= 60 and time_left < 70: 
+		return str(int(time_left)/60) + ":0" + str(int(time_left)%60)
 	else:
 		return str(int(time_left)/60) + ":" + str(int(time_left)%60) 
 	
