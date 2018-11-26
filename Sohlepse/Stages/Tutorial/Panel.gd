fe3bc362634c0362c46eb7bc165355fe09c9ff0d
PauloPaulo <ppqbambace@yahoo.com.br>
2018-11-26 19:53:24 -0200

    timer sem sprite

--- Sohlepse/Stages/Tutorial/Panel.gd
+++ Sohlepse/Stages/Tutorial/Panel.gd
@@ -1,10 +1,13 @@
 extends HBoxContainer
 
-onready var vector = ["E", "setas", "R", "P", "G", "F", "Player", "Circle", "X"]
+onready var vector = ["E", "setas", "R", "P", "G", "F", "Player", "Circle"]
 
-export(int, "E", "setas", "R", "P", "G", "F", "Player", "Circle", "X") var input = 0
+export(int, "E", "setas", "R", "P", "G", "F", "Player", "Circle") var input = 0
 export var label = ""
 
 func ready():
 	$Container/Label.text = label
-	$Container/Sprite.texture = load("res://Assets/"+str(vector[input])+".png")       
+	if input == 8:
+		$Container/Sprite.texture = null
+	else:
+		$Container/Sprite.texture = load("res://Assets/"+str(vector[input])+".png")       
