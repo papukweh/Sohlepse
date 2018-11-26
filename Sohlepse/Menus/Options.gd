9f12a5d13ff8313c9dc249c6e14c572c060a1c21
papukweh <nathalia.borin@usp.br>
2018-11-26 10:38:18 -0200

    Conserta bug do menu

--- Sohlepse/Menus/Options.gd
+++ Sohlepse/Menus/Options.gd
@@ -1,5 +1,6 @@
 extends Control
 onready var pause = false
+onready var enabled = false
 
 func _ready():
 	$Controls.options = true
@@ -11,9 +12,11 @@ func _ready():
 	
 	if get_parent().get_name() == "Tutoriais":
 		pause = true
+	else:
+		enabled = true
 
 func _process(delta):
-	if pause:
+	if pause and get_parent().get_node("PauseMenu").inOptions:
 		if Input.is_action_just_pressed("Pause") and get_parent().get_node("PauseMenu").inOptions:
 			get_parent().get_node("PauseMenu").inOptions = false
 			get_parent().get_node("PauseMenu").hide()
@@ -22,13 +25,14 @@ func _process(delta):
 			hide()
 		if get_parent().get_node("PauseMenu").inOptions and get_parent().get_node("PauseMenu").pressed[3]:
 			get_parent().get_node("PauseMenu").pressed[3] = false
-	if Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("ui_down") or Input.is_action_just_pressed("ui_left") or Input.is_action_just_pressed("ui_right"):
-		global.play_se(global.SE_MOVE,-15)
-		$Back/Label.set("custom_colors/font_color", Color(0,0,0))
-	if Input.is_action_just_pressed("ui_accept"):
-		global.play_se(global.SE_CHANGE, -5)		
-		$Back/Label.set("custom_colors/font_color", Color(0.86,0.96,0.92))
-		_on_Back_pressed()
+	if enabled or get_parent().get_node("PauseMenu").inOptions:
+		if Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("ui_down") or Input.is_action_just_pressed("ui_left") or Input.is_action_just_pressed("ui_right"):
+			global.play_se(global.SE_MOVE,-15)
+			$Back/Label.set("custom_colors/font_color", Color(0,0,0))
+		if Input.is_action_just_pressed("ui_accept"):
+			global.play_se(global.SE_CHANGE, -5)
+			$Back/Label.set("custom_colors/font_color", Color(0.86,0.96,0.92))
+			_on_Back_pressed()
 
 func _on_Back_pressed():
 	if pause:
